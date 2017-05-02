78cf5600db83a8c19a736863a8d293eae18895fe
justincalderon <justin.calderon@outlook.com>
2017-05-02 00:35:19 -0700

    If/else line 38

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ function getUserData() {
   var userPhone = document.getElementById('userPhone').value
 
   // store value for continent of birth and gender radio options
-  var userGenderSelection = document.getElementByClass('userGender')
-  var userBirthPlace = document.getElementByClass('userBirth')
+  // var userGender = document.getElementByClass('userGender')
+  // var userBirth = document.getElementByClass('userBirth')
 
   // create vars to store gender and birth
   var userGender, userBirth
@@ -34,16 +34,15 @@ function getUserData() {
   }
 
   // Gender
-  // loop through all gender options with class="userGender"
-  for(var i = 0; i < userGenderSelection.length; i++) {
+  // get gender values
     // if option was selected by the user, do this
-    if(userGenderSelection[i].checked) {
-      userGender = userGenderSelection[i].value
+    if(userGender[i].checked) {
+      userGender = userGender[i].value
       console.log(userGender);
     }
   }
   // stop function if no data is there for gender
-  if(userGender == undefined) {
+  if(userGender == '') {
     // add a class of failure to results if no value for gender
     results.className = "failure"
     // update the text content of results
@@ -73,7 +72,72 @@ function getUserData() {
   }
 
   // Birthplace
+  // loop through all options with class="userBirth"
+  for(var i = 0; i < userBirth.length; i++) {
+    // if option was selected by user, do this
+    if(userBirth[i].checked) {
+      // set value of answer to value of userBirthPlace
+      userBirth = userBirth[i].value
+      console.log(userBirth);
+    }
+  }
+  // stop function if no value is there for userBirthPlace
+  if(userBirth == undefined) {
+    // add a class of failure to results if no userBirth value
+    results.className = "failure"
+    // update the text content of results
+    results.textContent = "You forgot to select your birth place."
+    // stop function if no answer
+    return
+  }
+
+  // ON SUCCESS
+  // if everything passes add a class of success to results
+  results.className = "success"
+  // update the text content of results
+  results.textContent = "Thank you, updating your information now."
+
+  // create object for user profile data
+  var userProfile = {
+    name : userFirstName + " " + userLastName,
+    gender : userGender,
+    email : userEmail,
+    phone : userPhone,
+    birth : userBirth
+
+  }
+  // confirm existance of all user profile data
+  console.log(userProfile);
+
+  // run displayProfile after 1 second passing in new profile data to be displayed
+  setTimeout(function() {displayProfile(userProfile) }, 1000)
+
+  // display profile information
+  function displayProfile(userProfile) {
+    // make sure the suer data is being passed through
+    console.log(userProfile);
+
+    // select some html elements by id
+    var userName = document.getElementById('newUserName')
+    var userGender = document.getElementById('newUserGender')
+    var userEmail = document.getElementById('newUserEmail')
+    var userPhone = document.getElementById('newUserPhone')
+    var userBirth = document.getElementById('newUserBirth')
+
+    // change the text of these variables using the userProfile object
+    userName.innerText = "Welcome " + userProfile.name
+    userGender.innerText = "Gender: " + userProfile.gender
+    userEmail.innerText = "Email: " + userProfile.email
+    userPhone.innerText = "Phone: " + userProfile.phone
+    userBirth.innerText = "Birthplace " + userProfile.birth
+
+    // hide new profile form
+    // document.getElementById('newProfile').style.display = "hide"
+    // display updated profile data
+    // document.getElementById('updatedProfile').style.display = "block"
+
+  }
 
 
-  document.getElementById('sendDataButton').addEventListener('click', getUserData, false)
 }
+document.getElementById('sendDataButton').addEventListener('click', getUserData, false)
