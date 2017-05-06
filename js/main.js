1fd65ee6ee5c40aeef68caa2429f1ca10a34c2fc
justincalderon <justin.calderon@outlook.com>
2017-05-06 02:34:02 -0700

    JavaScript push to updateProfile

--- js/main.js
+++ js/main.js
@@ -6,13 +6,16 @@ function getUserData() {
   var userEmail = document.getElementById("userEmail").value
   var userPhone = document.getElementById("userPhone").value
 
+
   // create variables for gender selection
   var radio1 = document.getElementById("radio1").value
   var radio2 = document.getElementById("radio2").value
 
+
   // create variables for continent of birth
   var userBirth = document.getElementById("birthMenu")
   var userBirthSelect = userBirth.options[userBirth.selectedIndex].value
+
 
   // validate users selection
   // FIRST NAME
@@ -21,12 +24,16 @@ function getUserData() {
     results.textContent = "You forgot to add your first name."
     return
   };
+
+
   // LAST NAME
   if (userLastName == "") {
     results.className = "alert alert-danger"
     results.textContent = "You forgot to add your last name."
     return
   };
+
+
   // GENDER SELECTION
   if (document.getElementById("radio1").checked) {
     console.log(radio1);
@@ -37,28 +44,48 @@ function getUserData() {
     results.textContent = "Please choose a gender."
     return
   };
+
+
   // EMAIL
   if (userEmail == "") {
     results.className = "alert alert-danger"
     results.textContent = "You forgot to add your email."
     return
   };
+
+
   // PHONE
   if (userPhone == "") {
     results.className = "alert alert-danger"
     results.textContent = "You forgot to add your phone number."
     return
   };
+
+
   // CONTINENT OF BIRTH
 
   if (userBirthSelect == "Choose") {
     results.className = "alert alert-danger"
     results.textContent = "You forgot to choose your birthplace."
     return
-  }
+  };
   console.log(userBirthSelect);
 
+  // ON SUCCESS
 
+  // updateProfile
+  // create an object from user values
+  var userProfile = {
+    name: userFirstName + " " + userLastName,
+    email: userEmail,
+    phone: userPhone,
+  };
+  console.log(userProfile);
+
+  // display user object values in updatedProfile
+  newUserName.innerText = "Welcome " + userFirstName + " " + userLastName
+  newUserEmail.innerText = "Email : " + userEmail
+  newUserPhone.innerText = "Phone : " + userPhone
 
 
   // show updateProfile and hide newProfile
